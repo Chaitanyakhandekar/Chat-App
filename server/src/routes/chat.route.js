91c78491e3dcb5226f30bdfcaa284120065f44e9
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-02-02 19:53:39 +0530

    get user chats api endpoint completed

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import {userAuth} from "../middlewares/userAuth.middleware.js"
-import { createSingleChat } from "../controllers/chat.controller.js";
+import
+ {
+     createSingleChat,
+     getUserChats
+     } from "../controllers/chat.controller.js";
 
 const router = Router();
 
 // router.route("/send").post();
 // router.route("/receive").get();
 router.route("/:userId").post(userAuth,createSingleChat)
+router.route("/").get(userAuth,getUserChats)
 export default router;
