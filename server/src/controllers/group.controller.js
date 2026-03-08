2ba3897255c6e65dd4ce39fafd703fd73be374b0
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-03-08 15:33:18 +0530

    just updated code for auto avtar for users

--- server/src/controllers/group.controller.js
+++ server/src/controllers/group.controller.js
@@ -14,41 +14,41 @@ import { getIO } from "../sockets/socketInstance.js";
 import { socketEvents } from "../constants/socketEvents.js";
 
 
-const getGroupMembers = asyncHandler(async (req,res)=>{
-    const {id} = req.params;
+const getGroupMembers = asyncHandler(async (req, res) => {
+    const { id } = req.params;
 
-    if(!isValidObjectId(id)){
-        throw new ApiError(400,"Invalid GroupId")
+    if (!isValidObjectId(id)) {
+        throw new ApiError(400, "Invalid GroupId")
     }
 
     const groupMembers = await Chat.aggregate([
         {
-            $match:{
-                _id:new mongoose.Types.ObjectId(id)
+            $match: {
+                _id: new mongoose.Types.ObjectId(id)
             }
         },
         {
-            $lookup:{
-                from:"users",
-                localField:"participants",
-                foreignField:"_id",
-                as:"participants"
+            $lookup: {
+                from: "users",
+                localField: "participants",
+                foreignField: "_id",
+                as: "participants"
             }
         }
     ])
 
-    if(!groupMembers.length){
-       return res
+    if (!groupMembers.length) {
+        return res
             .status(200)
             .json(
-                new ApiResponse(200,[req.user._id],"No Members Yet.")
+                new ApiResponse(200, [req.user._id], "No Members Yet.")
             )
     }
 
     return res
         .status(200)
         .json(
-            new ApiResponse(200,groupMembers[0].participants,"Group Members Fetched Successfully.")
+            new ApiResponse(200, groupMembers[0].participants, "Group Members Fetched Successfully.")
         )
 })
 
