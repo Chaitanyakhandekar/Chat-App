aa0c037793e90aedec6e5a0eb3e432832102c66e
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-05-05 21:24:05 +0530

    created service for create single chat for reusability and to prevent tightly coupleness

--- server/src/services/chat.services.js
+++ server/src/services/chat.services.js
@@ -1,3 +1,4 @@
+import { Chat } from "../models/chat.model.js"
 import { Message } from "../models/message.model.js"
 import { User } from "../models/user.model.js"
 import { isChatExists } from "../utils/document existance check/chat.js"
@@ -5,6 +6,46 @@ import { isMessageExists } from "../utils/document existance check/message.js"
 import { isUserExists } from "../utils/document existance check/user.js"
 import { summarizeChat } from "./ai.service.js"
 import { getMessagesForSummary } from "./message.service.js"
+
+/**
+ * @description Service for creating new single chat
+ * @access User
+ * @param {ObjectId} currentUserId 
+ * @param {ObjectId} friendId 
+ * @returns New Chat Object
+ */
+const createSingleChatService = async (currentUserId, friendId) => {
+
+    const user = await isUserExists(currentUserId)
+    const friend = await isUserExists(friendId)
+
+    const isChatAlreadyExists = await Chat.findOne({
+        participants: { $all: [user._id, friend._id] },
+        isGroupChat: false
+    })
+
+    if (isChatAlreadyExists) {
+        throw new ApiError(400, "Chat Between These Users Already Exists.")
+    }
+
+    const newSingleChat = await Chat.create({
+        participants: [
+            user._id,
+            friend._id
+        ]
+    })
+
+    if (!newSingleChat) {
+        throw new ApiError(500, "Server Error While Creating New Single Chat.")
+    }
+
+    let newChat = await newSingleChat.populate("participants", "-password")
+
+    console.log("New Single Chat Created : ", newChat);
+
+    return newSingleChat;
+
+}
 
 /**
  * @description Service for summarizing chat conversations using AI
@@ -20,5 +61,6 @@ const summarizeChatService = async (chatId) => {
 }
 
 export {
-    summarizeChatService
+    summarizeChatService,
+    createSingleChatService
 }
